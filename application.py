15ab2118de89faf5392887421b4e7340d09936d1
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-11-06 17:58:16 +0100

    Add basic code to run the app

---
+++ application.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, request
+
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about_us')
+def render_about_us():
+    return render_template('about_us.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.error('Page not found: %s', (request.path))
+    return render_template('errors/404.html'), 404
+
+
+# start the server
+if __name__ == '__main__':
+    app.run(debug=True)
