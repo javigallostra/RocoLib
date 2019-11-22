6e30786b79bd7398436dbcc21776dd621928c7bc
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-11-22 14:02:23 +0100

    Add comment and remove magic number

--- helpers.py
+++ helpers.py
@@ -1,7 +1,11 @@
-def make_valid_json(filename):
+def make_valid_json(filename, skip_lines=1):
+    """
+    From a file with comma separated JSON like dictionaries
+    return a valid JSON object.
+    """
     valid_json = filename[:-3] + "_mod.txt"
     with open(filename, 'r') as contents:
-        save = contents.readlines()[1:]
+        save = contents.readlines()[skip_lines:]
     with open(valid_json, 'w') as contents:
         contents.write('{ "items": [')
     with open(valid_json, 'a') as contents:
