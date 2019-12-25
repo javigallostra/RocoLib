3816308e8b34342aa1795bad2e47669d68de2863
Juan Gallostra <juangallostra@gmail.com>
2019-12-25 12:05:59 +0100

    Add function to filter results

--- aws_controller.py
+++ aws_controller.py
@@ -1,6 +1,9 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 from decimal import Decimal
 import json
+import operator
+
 
 dynamodb = boto3.resource('dynamodb', 'eu-west-3')
 b_table = dynamodb.Table('Boulders')
@@ -18,3 +21,23 @@ def get_items():
 
 def put_item(item):
     return b_table.put_item(Item=item)
+
+
+def get_items_filtered(conditions=None):
+    if not conditions:
+        return get_items()
+    
+    first_expression = True
+    condition = None
+    for key, value in conditions.items():
+        if first_expression:
+            condition = Key(key).eq(value)
+            first_expression = False
+        else:
+            condition = operator.__and__(condition, Key(key).eq(value))
+    return json.dumps(b_table.scan(
+        FilterExpression=condition
+    ), default=decimal_default)
+
+
+print(get_items_filtered())
