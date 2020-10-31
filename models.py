4bc05a8f367dce6cb3c73bcadd581d601443c6fe
juangallostra <juangallostra@gmail.com>
2020-10-31 19:35:45 +0100

    Load and Save ticklist problems

--- models.py
+++ models.py
@@ -3,19 +3,24 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import firebase_controller
 
+TICKLIST = "ticklist"
+
 class User(UserMixin):
 
-    # def __init__(self, id, name, email, password, is_admin=False):
     def __init__(self, *initial_data, **kwargs):
         self.id = None
         self.name = None
         self.email = None
         self.password = None
         self.is_admin = False
+        self.ticklist = []
 
         for dictionary in initial_data:
             for key in dictionary:
-                setattr(self, key, dictionary[key])
+                if key == TICKLIST:
+                    self.load_ticklist(dictionary[key])
+                else:
+                    setattr(self, key, dictionary[key])
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
@@ -26,16 +31,22 @@ class User(UserMixin):
         return check_password_hash(self.password, password)
 
     def save(self):
-        # TODO: check if user already exists
         if not self.id:
             self.id = str(uuid.uuid1())
+        # Serialize ticklist problems
+        self.ticklist = [problem.serialize() for problem in self.ticklist]
         firebase_controller.save_user(self.__dict__)
+        # deserialize ticklist problems
+        self.load_ticklist(self.ticklist)
+
+    def load_ticklist(self, ticklist_data):
+        self.ticklist = [TickListProblem(problem) for problem in ticklist_data]
 
     @staticmethod
     def get_by_id(user_id):
         user_data = firebase_controller.get_user_data_by_id(user_id)
         if not user_data:
-            return None 
+            return None
         return User(user_data)
 
     @staticmethod
@@ -48,14 +59,26 @@ class User(UserMixin):
     def __repr__(self):
         return '<User {}>'.format(self.email)
 
-# For testing purposes
-users = []
 
-def get_user(email):
-    for user in users:
-        if user.email == email:
-            return user
-    return None
+class TickListProblem():
+
+    def __init__(self, *initial_data, **kwargs):
+        self.iden = None    
+        self.gym = None
+        self.section = None
+        self.is_done = False
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    def tick_problem(self):
+        self.is_done = True
+    
+    def serialize(self):
+        return self.__dict__
+
 
 if __name__ == '__main__':
     test_user = User(1, "test", "test@test.com", "pass", is_admin=False)
