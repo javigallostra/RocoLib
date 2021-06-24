1528ee0909d03b43c0359d8d2f2fa8edd8e70031
juangallostra <juangallostra@gmail.com>
2021-06-24 10:35:23 +0200

    Add boulder handler

---
+++ boulder_handler.py
@@ -0,0 +1,51 @@
+from werkzeug.utils import secure_filename
+
+import db.mongodb_controller as db_controller
+# pylint: disable=unused-wildcard-import
+from utils.utils import *
+from config import *
+from models import BoulderProblem
+
+import datetime
+
+
+def get_boulders_list(gym, filters, database) -> list:
+    """
+    Given a gym and a set of filters return the list of
+    boulders that match the specified criteria.
+    """
+    data = db_controller.get_boulders_filtered(
+        gym=gym,
+        database=database,
+        conditions=filters,
+        equals=EQUALS,
+        ranged=RANGE,
+        contains=CONTAINS
+    )
+    # Map and complete boulder data
+    boulders = [BoulderProblem(database, gym, b_info)
+                for b_info in data[ITEMS]]
+    return sorted(
+        boulders,
+        key=lambda boulder: datetime.datetime.strptime(
+            boulder.time, '%Y-%m-%dT%H:%M:%S.%f'),
+        reverse=True
+    )
+
+
+def get_boulder_by_name(gym, name, db) -> BoulderProblem:
+    boulder_data = db_controller.get_boulder_by_name(
+        gym=gym,
+        name=name,
+        database=db
+    )
+    return BoulderProblem(db, gym, boulder_data)
+
+
+def update_boulder_by_id(gym, boulder, db) -> bool:
+    return db_controller.update_boulder_by_id(
+        gym=gym,
+        boulder_id=boulder.get_id(),
+        data=boulder.serialize_for_db(),
+        database=db
+    )
