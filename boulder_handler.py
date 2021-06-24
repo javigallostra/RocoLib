56c318dec2aedecdc7b5f35a34cd9e6a909d3ffb
juangallostra <juangallostra@gmail.com>
2021-06-24 12:49:19 +0200

    Add function to load boulder from request

--- boulder_handler.py
+++ boulder_handler.py
@@ -9,21 +9,21 @@ from models import BoulderProblem
 import datetime
 
 
-def get_boulders_list(gym, filters, database) -> list:
+def get_boulders_list(filters, gym, db) -> list:
     """
     Given a gym and a set of filters return the list of
     boulders that match the specified criteria.
     """
     data = db_controller.get_boulders_filtered(
         gym=gym,
-        database=database,
+        database=db,
         conditions=filters,
         equals=EQUALS,
         ranged=RANGE,
         contains=CONTAINS
     )
     # Map and complete boulder data
-    boulders = [BoulderProblem(database, gym, b_info)
+    boulders = [BoulderProblem(db, gym, b_info)
                 for b_info in data[ITEMS]]
     return sorted(
         boulders,
@@ -33,7 +33,7 @@ def get_boulders_list(gym, filters, database) -> list:
     )
 
 
-def get_boulder_by_name(gym, name, db) -> BoulderProblem:
+def get_boulder_by_name(name, gym, db) -> BoulderProblem:
     boulder_data = db_controller.get_boulder_by_name(
         gym=gym,
         name=name,
@@ -42,10 +42,15 @@ def get_boulder_by_name(gym, name, db) -> BoulderProblem:
     return BoulderProblem(db, gym, boulder_data)
 
 
-def update_boulder_by_id(gym, boulder, db) -> bool:
+def update_boulder_by_id(boulder, gym, db) -> bool:
     return db_controller.update_boulder_by_id(
         gym=gym,
         boulder_id=boulder.get_id(),
         data=boulder.serialize_for_db(),
         database=db
     )
+
+def load_boulder_from_request(request, gym, db):
+    boulder_data = parse_boulder_from_request(request)
+    gym = boulder_data.pop('gym', gym)
+    return BoulderProblem(db, gym, boulder_data)
