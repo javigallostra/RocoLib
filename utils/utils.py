c8e40cadb8e4003fe154944306bb1c7efd4e4529
juangallostra <juangallostra@gmail.com>
2021-06-25 08:13:50 +0200

    Formating

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,17 @@
 import json
 from flask import url_for
 
+
 def load_boulder_from_request(request):
     """
     Replace boulder data from valid Python to valid JS
     """
     return json.loads(
-                request.form.get('boulder_data')
-                .replace('\'', '"')
-                .replace('True', 'true')
-                .replace('False', 'false'))
+        request.form.get('boulder_data')
+        .replace('\'', '"')
+        .replace('True', 'true')
+        .replace('False', 'false'))
+
 
 def get_wall_image(gym, section, walls_path, static_assets_path='static'):
     return url_for(
