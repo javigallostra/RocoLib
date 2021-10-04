15341d960b2978633beb99044afbd81ad6c3d5ea
juangallostra <juangallostra@gmail.com>
2021-10-04 12:23:17 +0200

    Add new argument to know the gym's images path, document

--- utils/add_new_gym.py
+++ utils/add_new_gym.py
@@ -3,15 +3,20 @@ import os
 
 # gym name as command line args -> gym_code, gym_name
 
-def add_new_gym(gym_code, gym_name):
+def add_new_gym(gym_code, gym_name, images_path):
     # Required steps:
     # 1. Create folder
+    # 2. Get wall images, tranform them if required,
+    #    and move them to the gym's folder
     # 2. Create collections and fill data
     # 3. Add gym to walls collection
     create_gym_folder(gym_code)
     pass
 
 def create_gym_folder(gym_code):
+    """
+    Create a new folder inside the wall images directory
+    """
     os.makedirs(f"./static/images/walls/{gym_code}")
 
 parser = argparse.ArgumentParser(description='Rocolib gym creation tool')
@@ -29,7 +34,14 @@ parser.add_argument(
     type=str, 
     required=True
 )
+parser.add_argument(
+    '-i',
+    '--images',
+    help='Path to the folder that contains gym wall images',
+    type=str, 
+    required=False
+)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    add_new_gym(args.code, args.name)
+    add_new_gym(args.code, args.name, args.images)
