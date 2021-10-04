928eb70edeef098c3224d5a9be74a9b9325bb85e
juangallostra <juangallostra@gmail.com>
2021-10-04 13:46:38 +0200

    Add logic to copy wall images from source path  to gym folder

--- utils/add_new_gym.py
+++ utils/add_new_gym.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 
-DEBUG = True
+from PIL import Image
+from shutil import copyfile
 
-# gym name as command line args -> gym_code, gym_name
+# gym name as command line args -> gym_code, gym_name, images_path
+
+walls_path = './static/images/walls'
+image_extensions = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')
 
 class Coordinates():
     def __init__(self, latitude=None, longitude=None):
@@ -11,16 +15,35 @@ class Coordinates():
         self.longitude = longitude
 
 def is_image(filename):
-    pass
+    """
+    Test if a file is an image
+    """
+    return filename.lower().endswith(image_extensions)
 
 def is_JPG(filename):
-    pass
+    """
+    Test if an image is in JPG format
+    """
+    return filename.endswith('.JPG')
 
-def convert_to_JPG(filename):
-    pass
+def convert_to_JPG(filename, gym_code=None, is_fullpath=True):
+    """
+    Convert an image to JPG
+    """
+    if not is_fullpath:
+        filename = f'{walls_path}/{gym_code}/{filename}'
+    im = Image.open(filename)
+    rgb_im = im.convert('RGB')
+    rgb_im.save(f'{os.path.splitext(filename)[0]}.JPG')
+    os.remove(filename) 
 
-def move_to_gym_dir(images_path, filename):
-    pass
+
+def move_to_gym_dir(filename, images_path, gym_code):
+    """
+    Move a file from a source directory (images_path + filename) to
+    the directory inside the application were wall images will be searched
+    """
+    return copyfile(f'{images_path}/{filename}', f'{walls_path}/{gym_code}/{filename}')
 
 def create_walls_collection(gym_code):
     pass
@@ -31,23 +54,22 @@ def create_boulders_collection(gym_code):
 def add_gym_to_gyms_list(gym_code, gym_name, radius=0.02, coordinates=Coordinates()):
     pass
 
-def add_new_gym(gym_code, gym_name, images_path, debug=False):
+def add_new_gym(gym_code, gym_name, images_path):
     # Required steps:
     # 1. Create folder
     # 2. Get wall images, tranform them if required,
     #    and move them to the gym's folder
     # 2. Create collections and fill data
     # 3. Add gym to walls collection
-    if not debug:
-        create_gym_folder(gym_code)
+    create_gym_folder(gym_code)
     if images_path:
         # Find all images, convert them to JPG and move them to the 
         # gym folder
         for filename in os.listdir(images_path):
             if is_image(filename):
-                if not is_JPG(filename):
-                    filename = convert_to_JPG(filename)
-                move_to_gym_dir(filename)
+                new_path = move_to_gym_dir(filename, images_path, gym_code)
+                if not is_JPG(new_path):
+                    convert_to_JPG(new_path, is_fullpath=True)
     # Add gym to DDBB
     create_walls_collection(gym_code)
     create_boulders_collection(gym_code)
@@ -84,4 +106,4 @@ parser.add_argument(
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    add_new_gym(args.code, args.name, args.images, debug=DEBUG)
+    add_new_gym(args.code, args.name, args.images)
