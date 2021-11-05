a9098484b14f6d06640d9196255d12b18ad2778b
juangallostra <juangallostra@gmail.com>
2021-11-05 10:11:16 +0100

    Include testing structure

---
+++ tests/__init__.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class BaseTestClass(unittest.TestCase):
+    """
+    BaseClass for testing
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every test
+        """
+        pass
+
+    def tearDown(self):
+        """
+        Tear down method that will run after every test
+        """
+        pass
