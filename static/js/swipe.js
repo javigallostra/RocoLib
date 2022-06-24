5b0948723b80b8a30d50e1cf5c04a4fab646f833
juangallostra <juangallostra@gmail.com>
2022-06-24 09:21:18 +0200

    refactor: simplify js code

--- static/js/swipe.js
+++ static/js/swipe.js
@@ -1,18 +1,18 @@
 var touchstartX = 0
 var touchendX = 0
 
-function changeProblem(swipe_left, swipe_right, current_id) {
+function changeProblem(swipe_left, swipe_right, current_id, gym_code) {
     if (touchendX < touchstartX) {
         // Here we should load previous problem from list
-        swipe_right(current_id);
+        swipe_right(current_id, gym_code);
     }
     if (touchendX > touchstartX) {
         // Here we should load next problem from list
-        swipe_left(current_id);
+        swipe_left(current_id, gym_code);
     }
 };
 
-function swipeInit(swipe_left, swipe_right, current_id) {
+function swipeInit(swipe_left, swipe_right, current_id, gym_code) {
 
     document.addEventListener('touchstart', e => {
         touchstartX = e.changedTouches[0].screenX;
@@ -20,10 +20,31 @@ function swipeInit(swipe_left, swipe_right, current_id) {
 
     document.addEventListener('touchend', e => {
         touchendX = e.changedTouches[0].screenX
-        changeProblem(swipe_left, swipe_right, current_id);
+        changeProblem(swipe_left, swipe_right, current_id, gym_code);
     });
 };
+
+function loadNext(problem_id, gym_code) {
+    loadProblem(problem_id, gym_code, "load_next");
+}
+
+function loadPrevious(problem_id, gym_code) {
+    loadProblem(problem_id, gym_code, "load_previous");
+}
+
+function loadProblem(problem_id, gym_code, endpoint) {
+    fetch(endpoint + "?gym=" + gym_code + "&id=" + problem_id, {})
+        .then(resp => resp.text())
+        .then(body => {
+            // Hacky Whacky
+            document.open();
+            document.write(body);
+            document.close();
+        });
+}
 
 window.touchstartX = touchstartX;
 window.touchendX = touchendX;
 window.swipeInit = swipeInit;
+window.loadNext = loadNext;
+window.loadPrevious = loadPrevious;
