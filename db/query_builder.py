dd4e3ac888356bde3452ccf5a0118584100eb3c1
juangallostra <juangallostra@gmail.com>
2022-08-04 17:24:49 +0200

    docs: type hinting

--- db/query_builder.py
+++ db/query_builder.py
@@ -7,61 +7,65 @@ def chaineable(f):
 
 class QueryBuilder:
     def __init__(self) -> None:
-        self.query = {}
+        self._query = {}
         self._root_key = '$and'
 
-    # db.user.find($and: [ {'the_key': { $exists: true }}, {'the_key': null}])
-    # {$and: [{rating: {$lte: 5}}, {rating: {$gt: 3}}, {creator: 'Juan G'}]}
+    def __repr__(self) -> str:
+        return str(self.query)
 
-    def _init_query(self):
-        self.query = {self._root_key: []}
+    def __str__(self) -> str:
+        return str(self.query)
 
-    def _is_query_empty(self):
-        return self._root_key not in self.query
+    @property
+    def query(self) -> dict:
+        return self._query
 
-    def _add_to_query(self, operator, property, value):
+    def _init_query(self) -> None:
+        self._query = {self._root_key: []}
+
+    def _is_query_empty(self)-> None:
+        return self._root_key not in self._query
+
+    def _add_to_query(self, operator: str, property: str, value)-> None:
         if self._is_query_empty():
             self._init_query()
-        self.query[self._root_key] += [{property: {operator: value}}]
+        self._query[self._root_key] += [{property: {operator: value}}]
 
-    def _reset_query(self):
-        self.query = {}
-
-    def get_query(self):
-        return self.query
+    def reset_query(self) -> None:
+        self._query = {}
 
     @chaineable
-    def contained_in(self, property, values):
+    def contained_in(self, property: str, values) -> None:
         self._add_to_query('$in', property, values)
 
     @chaineable
-    def not_contained_in(self, property, values):
+    def not_contained_in(self, property: str, values) -> None:
         self._add_to_query('$nin', property, values)
 
     @chaineable
-    def lower(self, property, value):
+    def lower(self, property: str, value) -> None:
         self._add_to_query('$lt', property, value)
 
     @chaineable
-    def lower_or_equal(self, property, value):
+    def lower_or_equal(self, property: str, value) -> None:
         self._add_to_query('$lte', property, value)
 
     @chaineable
-    def greater(self, property, value):
+    def greater(self, property: str, value) -> None:
         self._add_to_query('$gt', property, value)
 
     @chaineable
-    def greater_or_equal(self, property, value):
+    def greater_or_equal(self, property: str, value) -> None:
         self._add_to_query('$gte', property, value)
 
     @chaineable
-    def equal(self, property, value):
+    def equal(self, property: str, value) -> None:
         self._add_to_query('$eq', property, value)
 
     @chaineable
-    def not_equal(self, property, value):
+    def not_equal(self, property: str, value) -> None:
         self._add_to_query('$ne', property, value)
 
     @chaineable
-    def contains_text(self, property, value):
+    def contains_text(self, property: str, value) -> None:
         self._add_to_query('$regex', property, value)
