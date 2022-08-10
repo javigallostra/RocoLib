369c3865fc915e1ff6dd6687bc68d97298502c14
juangallostra <juangallostra@gmail.com>
2022-08-10 14:54:08 +0200

    feature: add new swiping js

--- static/js/swipe.js
+++ static/js/swipe.js
@@ -39,12 +39,16 @@ function swipeInit(swipe_left, swipe_right, current_id, gym_code) {
 //     loadProblem(problem_id, gym_code, "load_next");
 // }
 
-function loadPrevious(problem_id, gym_code) {
-    loadProblem(problem_id, gym_code, "load_previous");
-}
+// function loadPrevious(problem_id, gym_code) {
+//     loadProblem(problem_id, gym_code, "load_previous");
+// }
 
 function newLoadNext(problem_id, list_id, is_user_list) {
     newLoadProblem(problem_id, list_id, is_user_list, "load_next");
+}
+
+function newLoadPrevious(problem_id, list_id, is_user_list) {
+    newLoadNext(problem_id, list_id, is_user_list, "load_previous");
 }
 
 function newLoadProblem(problem_id, list_id, is_user_list, endpoint) {
@@ -61,17 +65,17 @@ function newLoadProblem(problem_id, list_id, is_user_list, endpoint) {
 
 }
 
-function loadProblem(problem_id, gym_code, endpoint) {
-    // Add more params to query: list_id, and any filter options
-    fetch(endpoint + "?gym=" + gym_code + "&id=" + problem_id + "&scroll=" + window.scrollY.toFixed(2), {})
-        .then(resp => resp.text())
-        .then(body => {
-            // Hacky Whacky
-            document.open();
-            document.write(body);
-            document.close();
-        });
-}
+// function loadProblem(problem_id, gym_code, endpoint) {
+//     // Add more params to query: list_id, and any filter options
+//     fetch(endpoint + "?gym=" + gym_code + "&id=" + problem_id + "&scroll=" + window.scrollY.toFixed(2), {})
+//         .then(resp => resp.text())
+//         .then(body => {
+//             // Hacky Whacky
+//             document.open();
+//             document.write(body);
+//             document.close();
+//         });
+// }
 
 window.touchstartX = touchstartX;
 window.touchendX = touchendX;
@@ -79,5 +83,6 @@ window.touchstartY = touchstartY;
 window.touchendY = touchendY;
 window.swipeInit = swipeInit;
 // window.loadNext = loadNext;
-window.loadPrevious = loadPrevious;
+// window.loadPrevious = loadPrevious;
+window.newLoadPrevious = newLoadPrevious;
 window.newLoadNext = newLoadNext;
