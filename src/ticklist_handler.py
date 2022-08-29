30c4094469e2f683399745e67724e03b23dfb4e2
juangallostra <juangallostra@gmail.com>
2022-08-29 13:24:59 +0200

    fix: avoid crashing if ticklist boulder not found & optimize ticklist load

--- src/ticklist_handler.py
+++ src/ticklist_handler.py
@@ -45,16 +45,39 @@ def load_user_ticklist(current_user, database: Database):
     Load a user's ticklist
     """
     # get boulders in ticklist and extra required values
-    boulder_list = [
-        mongodb_controller.get_ticklist_boulder(problem, database) for problem in current_user.ticklist
-        if mongodb_controller.get_ticklist_boulder(problem, database)['section'] in [
-            wall['image'] for wall in mongodb_controller.get_gym_walls(
-                mongodb_controller.get_ticklist_boulder(problem, database)['gym'],
-                database,
-                current_user.preferences.show_latest_walls_only
-            )]
-    ]
-    # filter by latest wall sets if flag is set
+    walls_cache = dict()
+    boulder_list = []
+    for ticklist_boulder in current_user.ticklist:
+        boulder = mongodb_controller.get_ticklist_boulder(ticklist_boulder, database)
+        if boulder:
+            # try to get walls from the cache
+            walls = walls_cache.get(
+                boulder['gym'], 
+                [
+                    wall['image'] for wall in mongodb_controller.get_gym_walls(
+                        boulder['gym'],
+                        database,
+                        current_user.preferences.show_latest_walls_only
+                    )
+                ]
+            )
+            walls_cache[boulder['gym']] = walls # update cache
+            # filter by valid subset of walls
+            if boulder['section'] in walls:
+                boulder_list.append(boulder)
+
+    # boulder_list = [
+    #     mongodb_controller.get_ticklist_boulder(problem, database) for problem in current_user.ticklist
+    #     if mongodb_controller.get_ticklist_boulder(problem, database)['section'] in [
+    #         wall['image'] for wall in mongodb_controller.get_gym_walls(
+    #             mongodb_controller.get_ticklist_boulder(problem, database)['gym'],
+    #             database,
+    #             current_user.preferences.show_latest_walls_only
+    #         )]
+    # ]
+
+    wall_name_cache = dict()
+    gym_name_cache = dict()
 
     unique_sections = dict()
     walls_list = []
@@ -66,10 +89,18 @@ def load_user_ticklist(current_user, database: Database):
         boulder['color'] = BOULDER_COLOR_MAP[boulder['difficulty']]
         if boulder['gym'] not in unique_sections.keys() and boulder['section'] not in unique_sections.values():
             unique_sections[boulder['gym']] = boulder['section']
+            gym_name = gym_name_cache.get(boulder['gym'], mongodb_controller.get_gym_pretty_name(boulder['gym'], database))
+            wall_name = wall_name_cache.get(
+                f'{boulder["gym"]}-{boulder["section"]}',
+                mongodb_controller.get_wall_name(boulder['gym'], boulder['section'], database)
+            )
+            # update caches
+            gym_name_cache[boulder['gym']] = gym_name
+            wall_name_cache[f'{boulder["gym"]}-{boulder["section"]}'] = wall_name
             walls_list.append({
-                'gym_name': mongodb_controller.get_gym_pretty_name(boulder['gym'], database),
+                'gym_name': gym_name,
                 'image': boulder['section'],
-                'name': mongodb_controller.get_wall_name(boulder['gym'], boulder['section'], database)
+                'name': wall_name
             })
     return boulder_list, walls_list
 
